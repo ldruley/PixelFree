93f0ddc4b50ae2fd080b970f243707af45b9ea3c
gusjavi <gujavier@csumb.edu>
2025-12-07 21:43:50 -0800

    final touches on css end, and flow of frame frontend

--- frontend_Frame/src/App.tsx
+++ frontend_Frame/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext'
 import ProtectedRoute from './components/ProtectedRoute'
 import LoginPage from './pages/LoginPage'
@@ -8,37 +8,22 @@ import WelcomePage from './pages/WelcomePage'
 function App() {
     return (
         <AuthProvider>
-                <Router>
-                    <Routes>
-                        <Route path="/welcome" element={<WelcomePage />} />
-                        <Route
-                            path="/"
-                            element={
-                                <ProtectedRoute>
-                                    <PlayerPage />
-                                </ProtectedRoute>
-                            }
-                        />
-
-                        <Route
-                            path="/*"
-                            element={
-                                <div>
-
-                                    <main>
-                                        <Routes>
-                                            <Route path="/login" element={<LoginPage />} />
-
-                                        </Routes>
-                                    </main>
-                                </div>
-                            }
-                        />
-                    </Routes>
-                </Router>
-
+            <Router>
+                <Routes>
+                    <Route path="/" element={<WelcomePage />} />
+                    <Route
+                        path="/player"
+                        element={
+                            <ProtectedRoute>
+                                <PlayerPage />
+                            </ProtectedRoute>
+                        }
+                    />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="*" element={<Navigate to="/" replace />} />
+                </Routes>
+            </Router>
         </AuthProvider>
     )
 }
-
 export default App
